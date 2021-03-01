9bf64eeee8856d2208995f18a5faf9ffd2265ab2
Meghana Bhat <42279550+meghanabhat29@users.noreply.github.com>
2021-03-01 22:15:55 +0530

    Errors yet to be resolved

---
+++ config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"log"
+
+	"github.com/gocql/gocql"
+)
+
+//KeyspaceConnection ...
+type KeyspaceConnection struct {
+	cluster *gocql.ClusterConfig
+	session *gocql.Session
+}
+
+var connection KeyspaceConnection
+
+//Setupconnection ...
+func Setupconnection() {
+	var err error
+	connection.cluster = gocql.NewCluster("127.0.0.1")
+	connection.cluster.Consistency = gocql.Quorum
+	connection.cluster.Keyspace = "/home/meghanar/cassandra/apache-cassandra-3.11.10/data/data/playlist"
+	connection.session, err = connection.cluster.CreateSession()
+	if err != nil {
+		log.Println(err)
+	}
+	//defer connection.session.Close()
+	//return
+}
